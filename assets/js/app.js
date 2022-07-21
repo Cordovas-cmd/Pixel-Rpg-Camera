fdd4a0bbede77ae862c36f42d6e84d622ebd1830
samuel cordova <cordovas1337@gmail.com>
2022-07-21 08:13:51 -0500

    added some starting css to the page will be adding in more elements and styling as we go along

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,13 @@
 // Grab elements from the dom.
 var character = document.querySelector(".character");
 var map = document.querySelector(".map");
+
+
+// character state
+
+var x =0;
+var y = 0;
+var held_directions = []; // arrow keys being held down;
+
+var speed = 1; // pixels per frame
+
